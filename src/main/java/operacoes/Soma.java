f2312c05a99fdb833d9eebaa1bdceb7f4057b5ef
NicolasZampieri67 <nicolaszampieri43@gmail.com>
2026-05-07 17:10:01 -0300

    refactor: altera a forma como o usuário digita os dados

--- src/main/java/operacoes/Soma.java
+++ src/main/java/operacoes/Soma.java
@@ -1,12 +1,11 @@
 package operacoes;
 
-import java.util.Scanner;
+import numeros.EntradaNumeros;
 
 public class Soma {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-         int x = scanner.nextInt();
-         int y = scanner.nextInt();
-        System.out.println(x + y);
+        EntradaNumeros entradaNumeros = new EntradaNumeros();
+        entradaNumeros.pedirNumeros();
+        System.out.println(entradaNumeros.x + entradaNumeros.y);
     }
 }
